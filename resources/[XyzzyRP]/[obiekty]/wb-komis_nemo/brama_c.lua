60b528a23cdcb13576f22741ef2694418bd53596
Łukasz Biegaj <lukasz.biegaj@gmail.com>
2013-08-23 09:40:04 +0200

    Zakomentowanie kodu specyficznego dla serwera LSS-RP

--- resources/[XyzzyRP]/[obiekty]/wb-komis_nemo/brama_c.lua
+++ resources/[XyzzyRP]/[obiekty]/wb-komis_nemo/brama_c.lua
@@ -1,7 +1,10 @@
 function menu_brama()
 	local c=getElementData(localPlayer,"character")
 	if not c or not c.id then return end
-	if tonumber(c.id)~=13743 and tonumber(c.id)~=12861 then
+	-- @todo w tym miejscu uzywane bylo sztywne przypisanie bramy do konkretnych graczy
+	-- @todo zmienic na wlasciwe ID postaci lub zmienic na mechanizm sprawdzajacy uprawnienia
+	-- @todo w bazie danych
+	if true or (tonumber(c.id)~=13743 and tonumber(c.id)~=12861) then
 		outputChatBox("Nie masz klucza do bramy.", 255,0,0,true)
 		return
 	end
