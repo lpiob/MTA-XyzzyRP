078a0abeddf7c14b74371bdc3844243afbbbffe7
Łukasz Biegaj <lukasz.biegaj@gmail.com>
2013-12-12 14:23:37 +0100

    formatowanie + dokumentacja

--- resources/[XyzzyRP]/lss-core/auth.lua
+++ resources/[XyzzyRP]/lss-core/auth.lua
@@ -6,8 +6,11 @@
 @license GPLv2
 ]]--
 
-
-
+--[[ Event wywolywany przez klienta, w celu weryfikacji autoryzacji gracza
+ W przypadku błędnej autoryzacji, poprzez event onAuthResult odsyłana jest odpowiedź.
+ W przypadku pozytywnej auotoryzacji następuje proces logowania, sprawdzenia banów, multikont,
+ wysłanie potwierdzenie udanej autoryzacji do klienta, oraz wywoływana jest funkcja pobierająca
+ postacie gracza ]]--
 addEvent("onAuthRequest",true)
 addEventHandler("onAuthRequest", root, function(login,password)
     if (not login or not password) then
@@ -15,6 +18,7 @@ addEventHandler("onAuthRequest", root, function(login,password)
 	triggerClientEvent(source, "onAuthResult", root, retval)
 	return
     end
+
     local query=string.format("SELECT id,login,gp,premium>NOW() premium, premium premium_data,blokada_ooc>NOW() blokada_ooc,blokada_ooc blokada_ooc_data,blokada_bicia>NOW() blokada_bicia,blokada_bicia blokada_bicia_data,blokada_aj,blokada_pm>NOW() blokada_pm,blokada_pm blokada_pm_data,voice_allowed,uo_sw,uo_sb,uo_cp FROM lss_users WHERE login='%s' AND hash='%s' LIMIT 1", exports.DB:esc(login), md5(string.lower(login) .. "MRFX_01" .. password))
     local wyniki=exports.DB:pobierzWyniki(query)
     if (not wyniki or not wyniki.id) then
@@ -33,15 +37,13 @@ addEventHandler("onAuthRequest", root, function(login,password)
 	end
 
     end
-	-- rejestrujemy logowanie
-	exports.DB:zapytanie("INSERT INTO lss_accesslog SET serial='".. exports.DB:esc(getPlayerSerial(source)) .."',ip=INET_ATON('".. exports.DB:esc(getPlayerIP(source)) .."'),user_id="..tonumber(wyniki.id).." ON DUPLICATE KEY UPDATE ts=NOW(),ip=INET_ATON('".. exports.DB:esc(getPlayerIP(source)) .."');")
+    -- rejestrujemy logowanie
+    exports.DB:zapytanie("INSERT INTO lss_accesslog SET serial='".. exports.DB:esc(getPlayerSerial(source)) .."',ip=INET_ATON('".. exports.DB:esc(getPlayerIP(source)) .."'),user_id="..tonumber(wyniki.id).." ON DUPLICATE KEY UPDATE ts=NOW(),ip=INET_ATON('".. exports.DB:esc(getPlayerIP(source)) .."');")
 
     setElementData(source,"auth:uid", tonumber(wyniki.id))
 
-
-	-- sprawdzamy bany
-	if (isPlayerBanned(source)) then return end
-
+    -- sprawdzamy bany
+    if (isPlayerBanned(source)) then return end
 
     -- sprawdzamy mk
     local mk=exports.DB2:pobierzWyniki("select count(distinct user_id) ile_kont from lss_accesslog where serial=? and datediff(now(),ts)<=7", getPlayerSerial(source))
@@ -50,20 +52,18 @@ addEventHandler("onAuthRequest", root, function(login,password)
       return
     end
 
-
-
-	-- finalizujemy logowanie
-    
+    -- finalizujemy logowanie
 
     setElementData(source,"auth:login", wyniki.login)
-	setElementData(source,"GP", tonumber(wyniki.gp))
+    setElementData(source,"GP", tonumber(wyniki.gp))
+
     if (tonumber(wyniki.blokada_ooc)>0) then
 	setElementData(source,"kary:blokada_ooc", wyniki.blokada_ooc_data)
     end
+
     if (tonumber(wyniki.premium)>0) then
 	setElementData(source,"premium", wyniki.premium_data)
     end
-
 
     if (tonumber(wyniki.blokada_bicia)>0) then
 	setElementData(source,"kary:blokada_bicia", wyniki.blokada_bicia_data)
@@ -71,7 +71,6 @@ addEventHandler("onAuthRequest", root, function(login,password)
     if (tonumber(wyniki.blokada_pm)>0) then
 	setElementData(source,"kary:blokada_pm", wyniki.blokada_pm_data)
     end
-
 
     if (wyniki.blokada_aj and tonumber(wyniki.blokada_aj)>0) then
 	setElementData(source,"kary:blokada_aj", wyniki.blokada_aj)
@@ -92,13 +91,6 @@ addEventHandler("onAuthRequest", root, function(login,password)
     if (wyniki.uo_cp and tonumber(wyniki.uo_cp)>0) then
 		setElementData(source,"uo_cp", true)
     end
-
-
-
-
-
-
-
 
     local retval={ success=true }
     triggerClientEvent(source, "onAuthResult", root, retval)
@@ -129,44 +121,45 @@ function translit(tekst)
 end
 																			
 local womanSkins = {
-	[9]=true,
-	[11]=true,
-	[12]=true,
-	[13]=true,
-	[31]=true,
-	[40]=true,
-	[63]=true,
-	[69]=true,
-	[86]=true,
-	[90]=true,
-	[91]=true,
-	[92]=true,
-	[93]=true,
-	[131]=true,
-	[141]=true,
-	[150]=true,
-	[151]=true,
-	[152]=true,
-	[169]=true,
-	[190]=true,
-	[191]=true,
-	[192]=true,
-	[193]=true,
-	[211]=true,
-	[214]=true,
-	[216]=true,
-	[224]=true,
-	[226]=true,
-	[234]=true,
-	[263]=true,
-	[298]=true,
+    [9]=true,
+    [11]=true,
+    [12]=true,
+    [13]=true,
+    [31]=true,
+    [40]=true,
+    [63]=true,
+    [69]=true,
+    [86]=true,
+    [90]=true,
+    [91]=true,
+    [92]=true,
+    [93]=true,
+    [131]=true,
+    [141]=true,
+    [150]=true,
+    [151]=true,
+    [152]=true,
+    [169]=true,
+    [190]=true,
+    [191]=true,
+    [192]=true,
+    [193]=true,
+    [211]=true,
+    [214]=true,
+    [216]=true,
+    [224]=true,
+    [226]=true,
+    [234]=true,
+    [263]=true,
+    [298]=true,
 }
 
+
+--[[ Funkcja pobiera dane gracza, przetwarza je i przesyła do niego.
+     Wywoływana jest w procesie autoryzacji ]]--
 function auth_fetchPlayerCharacters(player,userid)
-    -- onCharacterSetReceived
---    local query=string.format("SELECT id,imie,nazwisko,skin,tytul,lastpos,hp,ar,money,newplayer,fingerprint FROM lss_characters WHERE userid=%d;", userid)
     local query=string.format("SELECT c.id,c.opis,c.imie,c.lastseen,c.nazwisko,c.skin,c.stylewalki,c.premiumskin,c.dead,c.tytul,c.lastpos,c.hp,c.ar,c.money,c.newplayer,c.fingerprint,c.energy,c.satiation,c.ab_spray,c.stamina,cc.co_id,cc.rank co_rank,cc.skin co_skin,co.name co_name,cr.name co_rank_name FROM lss_characters c LEFT JOIN lss_character_co cc ON cc.character_id=c.id LEFT JOIN lss_co co ON co.id=cc.co_id LEFT JOIN lss_co_ranks cr ON cr.co_id=cc.co_id AND cc.rank=cr.rank_id WHERE c.userid=%d AND c.accepted=1 ORDER BY c.dead ASC,c.lastseen DESC,id DESC", userid)
-    local characters=exports.DB:pobierzTabeleWynikow(query)
+    local characters=exports.DB:pobierzTabeleWynikow(query) -- @todo przerobic na DB2
     -- kick jesli gracz nie ma postaci
     if (not characters or #characters<1) then
 	kickPlayer(player,"Twoje konto nie ma żadnych przypisanych postaci.")
@@ -176,22 +169,22 @@ function auth_fetchPlayerCharacters(player,userid)
     -- przetwarzamy dane
     -- rozbijamy lastpos=1,2,3,4,... na lastpos={1,2,3,4}
     for i,v in ipairs(characters) do
-	  if (getElementData(player,"premium") and characters[i].premiumskin and tonumber(characters[i].premiumskin) and tonumber(characters[i].premiumskin)>0) then
-		  characters[i].skin=tonumber(characters[i].premiumskin)
-	  end
-	characters[i].imie=translit(characters[i].imie)
-	characters[i].nazwisko=translit(characters[i].nazwisko)
-	characters[i].lastpos=split(v.lastpos,",")
-	local plec = womanSkins[tonumber(characters[i].skin)]
-	if plec then
-		characters[i].plec = "woman"
-	else
-		characters[i].plec = "man"
-	end
-	for i2,v2 in ipairs(characters[i].lastpos) do
-	    characters[i].lastpos[i2]=tonumber(v2)
-	end
+        if (getElementData(player,"premium") and characters[i].premiumskin and tonumber(characters[i].premiumskin) and tonumber(characters[i].premiumskin)>0) then
+            characters[i].skin=tonumber(characters[i].premiumskin)
+        end
+        characters[i].imie=translit(characters[i].imie)
+        characters[i].nazwisko=translit(characters[i].nazwisko)
+        characters[i].lastpos=split(v.lastpos,",")
+        local plec = womanSkins[tonumber(characters[i].skin)]
+        if plec then
+            characters[i].plec = "woman"
+        else
+            characters[i].plec = "man"
+        end
+        for i2,v2 in ipairs(characters[i].lastpos) do
+            characters[i].lastpos[i2]=tonumber(v2)
+        end
     end
-    
+
     triggerClientEvent(player, "onCharacterSetReceived", root, characters)
 end
